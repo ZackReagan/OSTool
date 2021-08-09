582eebcdcb4e13cb5002da8fb100b14c2d45773d
EverythingTech <86342539+EverythingTechTutorials@users.noreply.github.com>
2021-08-09 16:03:54 -0400

    Add files via upload

---
+++ OSToolWPF/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace OSTool
+{
+    public partial class App : Application
+    {
+
+    }
+}
---
+++ OSToolWPF/OSToolWPF.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace OSTool
+{
+    public partial class OSToolWPF : Window
+    {
+        public OSToolWPF()
+        {
+            InitializeComponent();
+            DataContext = new OSToolViewModel(this);
+        }
+    }
+}
